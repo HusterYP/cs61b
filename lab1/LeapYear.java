ea11aa131b131c3a206e58e73f05d6f433b600df
yuanping <yuanping.0@bytedance.com>
2023-07-15 18:47:49 +0800

    lab1
    Change-Id: Icd34d5aca1818bba4f45fdf9b05f6ee5859522b5

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,14 @@ public class LeapYear {
         }
     }
 
+    /**
+     * @param year to be analyzed
+     * @return true if year is leap year or false if not
+     */
+    private static Boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
